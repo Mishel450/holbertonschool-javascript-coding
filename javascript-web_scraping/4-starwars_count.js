6e833b4ab311bd9011302954fc454deaa5d4d024
Mishel450 <tomasmishel18@gmai.com>
2023-09-18 09:03:24 -0700

    task-4 semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,20 +3,20 @@
 const url = process.argv[2];
 
 const request = require('request');
-let contador = 0
+let contador = 0;
 
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   }
   const bodyJson = JSON.parse(body).results;
-    for (const the_film of bodyJson) {
-      for (const i of the_film.characters) {
-        if (i.includes(`/people/18/`)) {
-          contador++
-        }
+  for (const thefilm of bodyJson) {
+    for (const i of thefilm.characters) {
+      if (i.includes('/people/18/')) {
+        contador++;
       }
     }
-  console.log(contador)
   }
+  console.log(contador);
+}
 );
