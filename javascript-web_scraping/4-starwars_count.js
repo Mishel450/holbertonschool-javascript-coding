ba60879fbdaf3b334ddf4edcaeafbed317a16d2a
Mishel450 <tomasmishel18@gmai.com>
2023-09-18 09:02:20 -0700

    task-4 done

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,28 +2,21 @@
 
 const url = process.argv[2];
 
-
 const request = require('request');
 let contador = 0
 
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-  } else {
-    for (let i = 0; i < body.count; i++) {
-      let characters = body.results[i].characters
-      for (j in characters) {
-        if (j == "https://swapi-api.hbtn.io/api/people/18") {
-        contador += 1
+  }
+  const bodyJson = JSON.parse(body).results;
+    for (const the_film of bodyJson) {
+      for (const i of the_film.characters) {
+        if (i.includes(`/people/18/`)) {
+          contador++
         }
       }
     }
-    console.log(contador)
+  console.log(contador)
   }
-});
-
-for (let i in request) {
-  if (request.characters == "https://swapi-api.hbtn.io/api/people/18/") {
-    count += 1
-  }
-}
+);
